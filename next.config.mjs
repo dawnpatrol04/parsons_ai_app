bce751effd73eb15ec00b716664becde9d24d780
dawnpatrol04 <dawnpatrol04@gmail.com>
2024-09-11 20:41:02 -0400

    test deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    output: 'export',
+    images: {
+      unoptimized: true,
+    },
+    basePath: '/parsons_ai_app',
+    assetPrefix: '/parsons_ai_app/',
+  };
+  
+  export default nextConfig;
