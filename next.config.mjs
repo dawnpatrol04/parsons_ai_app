afa53b8ab9750fd2350dd10cb94f8c17691a3c21
dawnpatrol04 <dawnpatrol04@gmail.com>
2024-09-13 08:21:26 -0400

    asdf

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
   images: {
     unoptimized: true, // Since GitHub Pages can't optimize images dynamically
   },
-  basePath: process.env.NODE_ENV === 'production' ? '/parsons_ai_app' : '', // Base path for the project
+  basePath: process.env.NODE_ENV === 'production' ? '' : '', // Base path for the project
   // assetPrefix: process.env.NODE_ENV === 'production' ? '/parsons_ai_app/' : '', // Prefix for assets
   eslint: {
     ignoreDuringBuilds: true, // Skip linting during the build
