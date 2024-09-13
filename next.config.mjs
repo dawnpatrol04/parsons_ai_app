4c69e1ca76023e81fbee9f0b024a0f306a1bce47
dawnpatrol04 <dawnpatrol04@gmail.com>
2024-09-13 08:41:33 -0400

    asdf

--- next.config.mjs
+++ next.config.mjs
@@ -4,8 +4,8 @@ const nextConfig = {
   images: {
     unoptimized: true, // Since GitHub Pages can't optimize images dynamically
   },
-  basePath: process.env.NODE_ENV === 'production' ? '' : '', // Base path for the project
-  assetPrefix: process.env.NODE_ENV === 'production' ? '' : '', // Prefix for assets
+  // basePath: process.env.NODE_ENV === 'production' ? '' : '', // Base path for the project
+  // assetPrefix: process.env.NODE_ENV === 'production' ? '' : '', // Prefix for assets
   eslint: {
     ignoreDuringBuilds: true, // Skip linting during the build
   },
